0b8f9e2aecfbb05e089e05be9fd607280210defe
Kristian Tahop <117333509+ktahop@users.noreply.github.com>
2023-09-13 14:50:16 -0400

    Fix hero/header brightness

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Nav from "@/components/Nav"
 const Home = () => {
   return (
     <main className="bg-black">
-      <div className="container relative mx-auto px-[1rem] bg-hero-mobile md:bg-hero-tablet lg:bg-hero-desktop bg-contain bg-center lg:bg-right bg-no-repeat h-[50vh] text-gray">
+      <div className="container relative mx-auto px-[1rem] bg-hero-mobile md:bg-hero-tablet lg:bg-hero-desktop bg-contain bg-center lg:bg-right bg-no-repeat h-[70vh] text-gray">
         <Nav />
         <div className="h-full flex items-center justify-center lg:justify-start">
           <div className="w-[400px] text-center lg:text-left">
